011a06491f5b215254eaca9fe2995ce4fdd7a5b7
Shay K. Stage <ShayKStage@protonmail.com>
2023-11-03 18:30:01 +0100

    Create command luksmount

---
+++ src/bin/luksmount/cli.rs
@@ -0,0 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser, Debug, Clone)]
+#[command(author, version, about = "Mount a LUKS encrypted filesystem", long_about = None)]
+pub(crate) struct Cli {
+    /// The block device to mount (eg. /dev/sdb1)
+    pub(crate) dev: String,
+    /// The directory to use as the mountpoint (eg. /mnt)
+    pub(crate) mnt: String,
+    /// The filesystem to mount it as (eg. ext4, btrfs, ntfs)
+    #[arg(default_value = "ext4")]
+    pub(crate) fstype: String,
+    /// Try to create the mount directory
+    #[arg(short, long)]
+    pub(crate) mkdir: bool,
+}
--- src/bin/luksmount/main.rs
+++ src/bin/luksmount/main.rs
@@ -1,3 +1,57 @@
+use clap::Parser;
+use std::process::Command;
+
+mod cli;
+
 fn main() {
-    println!("Hello world!")
+    let cli = cli::Cli::parse();
+    dbg!(cli.clone());
+
+    let device_mapper = "luksmount-".to_string() + cli.mnt.replace("/", "__").as_str();
+    dbg!(device_mapper.clone());
+
+    match Command::new("cryptsetup")
+        .arg("open")
+        .arg(cli.dev.clone())
+        .arg(device_mapper.clone())
+        .status()
+    {
+        Ok(status) => {
+            if !status.success() {
+                eprintln!("Failed to open encrypted volume {}", cli.dev);
+                std::process::exit(status.code().unwrap_or(-1))
+            }
+        }
+        Err(error) => {
+            eprintln!("Command \"cryptsetup\" failed with error: {}", error);
+            std::process::exit(-1)
+        }
+    }
+
+    let mut mount_cmd = Command::new("mount");
+    mount_cmd
+        .arg("-t")
+        .arg(cli.fstype)
+        .arg(format!("/dev/mapper/{}", device_mapper.clone()))
+        .arg(cli.mnt.clone());
+
+    if cli.mkdir {
+        mount_cmd.arg("--mkdir");
+    }
+
+    match mount_cmd.status() {
+        Ok(status) => {
+            if !status.success() {
+                eprintln!(
+                    "Failed to mount volume {} at {} via mapper volume {}",
+                    cli.dev, cli.mnt, device_mapper
+                );
+                std::process::exit(status.code().unwrap_or(-1))
+            }
+        }
+        Err(error) => {
+            eprintln!("Command \"mount\" failed with error: {}", error);
+            std::process::exit(-1)
+        }
+    }
 }
