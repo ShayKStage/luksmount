df8cbaae06d27f4cc5049fd48ae0f712fca227bc
Shay K. Stage <ShayKStage@protonmail.com>
2023-11-04 15:42:31 +0100

    Update wrong about in luksumount/cli

--- src/bin/luksumount/cli.rs
+++ src/bin/luksumount/cli.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 
 #[derive(Parser, Debug, Clone)]
-#[command(author, version, about = "Mount a LUKS encrypted filesystem", long_about = None)]
+#[command(author, version, about = "Unmount a LUKS encrypted filesystem", long_about = None)]
 pub(crate) struct Cli {
     /// The mount directory (eg. /mnt)
     pub(crate) mnt: String,
